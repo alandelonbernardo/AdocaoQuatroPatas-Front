07c4b7f201526a3ef40698a7074103564120ae52
Alandelon Oliveira <alandelonm84@gmail.com>
2025-05-31 02:54:15 -0300

    CRUD Animals atualizado, adição de upload de imagens e ajuste nos inputs

--- src/pages/Animals/CreatePage.vue
+++ src/pages/Animals/CreatePage.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <q-page>
     <div class="q-pa-md">
       <h6 class="q-ma-xs">Cadastrar Animal</h6>
@@ -33,14 +33,133 @@
       </div>
     </div>
   </q-page>
+</template> -->
+<template>
+  <q-page>
+    <div class="q-pa-md">
+      <q-card class="q-pa-md q-pb-xl">
+        <q-card-header class="justify">
+          <q-toolbar-title>
+            <q-icon name="help" class="q-m-sm"/>
+            Cadastrar Animal
+          </q-toolbar-title>
+        </q-card-header>
+        <q-card-content>
+          <div class="row full-width">
+            <q-form
+            class="row full-width"
+            >
+              <div class="col-md-12 col-sm-12 col-xs-12 q-pa-xs q-mt-md q-mb-md">
+                <q-uploader
+                    @uploaded="arquivoEnviado"
+                    url="http://localhost:8000/api/upload"
+                    class="full-width"
+                    max-files="1"
+                    auto-upload
+                />
+              </div>
+              <div class="col-md-6 col-xs-6 q-pa-xs">
+                <q-input
+                        lazy-rules
+                        :rules="[ val => val && val.length > 0 || 'O nome não pode ser vazio!']"
+                        label="Nome"
+                        type="text"
+                        v-model="form.name"
+                        filled
+                />
+              </div>
+              <div class="col-md-6 col-xs-6 q-pa-xs">
+                <q-input
+                        lazy-rules
+                        :rules="[ val => val && val.length > 0 || 'A espécie não pode ser vazia!']"
+                        label="Espécie"
+                        type="text"
+                        v-model="form.specie"
+                        filled
+                />
+              </div>
+              <div class="col-md-12 col-xs-6 q-pa-xs">
+                <q-input
+                        lazy-rules
+                        :rules="[ val => val && val.length > 0 || 'A raça não pode ser vazia!']"
+                        label="Raça"
+                        type="text"
+                        v-model="form.breed"
+                        filled
+                />
+              </div>
+              <div class="col-md-3 col-sm-6 col-xs-6 col-xs-3 q-pa-xs">
+                <q-input
+                        lazy-rules
+                        :rules="[ val => val && val.length > 0 || 'O tamanho não pode ser vazio!']"
+                        label="Tamanho"
+                        type="text"
+                        suffix="Cm"
+                        v-model="form.size"
+                        filled
+                />
+              </div>
+              <div class="col-md-3 col-sm-6 col-xs-6 col-xs-3 q-pa-xs">
+                <q-input
+                        lazy-rules
+                        :rules="[ val => val && val.length > 0 || 'A idade não pode ser vazia!']"
+                        label="Idade"
+                        type="text"
+                        suffix="Anos"
+                        v-model="form.age"
+                        filled
+                />
+              </div>
+              <div class="col-md-6 col-xs-12 col-sm-12 q-pa-xs">
+                <q-select
+                        lazy-rules
+                        :rules="[ val => val && val.length > 0 || 'O status não pode ser vazio!']"
+                        label="Status"
+                        v-model="form.status"
+                        :options="optionsSatus"
+                        filled
+                />
+              </div>
+              <div class="col-md-12 col-sm-12 col-xs-12  col-xs-3 q-pa-xs">
+                <q-input
+                        lazy-rules
+                        :rules="[ val => val && val.length > 0 || 'A descrição não pode ser vazia!']"
+                        label="Descrição"
+                        v-model="form.description"
+                        filled
+                />
+              </div>
+              <div class="col-md-12 col-xs-12 q-pa-xs">
+                <q-btn
+                      @click="saveData()"
+                      type="submit"
+                      color="green-6"
+                      label="SALVAR"
+                      class="full-width"
+                />
+              </div>
+            </q-form>
+          </div>
+        </q-card-content>
+      </q-card>
+    </div>
+  </q-page>
 </template>
+
 
 <script>
 import { ref } from 'vue';
 import AnimalService from 'src/services/AnimalService'
+import { useQuasar } from 'quasar';
 
 export default {
   setup () {
+    const $q = useQuasar()
+    const optionsSatus = [
+      'Disponível',
+      'Adotado',
+    ]
+
     const form = ref({
       name: '',
       specie: '',
@@ -48,18 +167,33 @@ export default {
       size: '',
       age: '',
       photo: '',
-      status: '',
+      status: 'Selecione...',
       description: '',
 
     })
 
     return {
-      form
+      $q,
+      form,
+      optionsSatus,
     }
   },
+  // mounted() {
+  //   this.getUsers()
+  // },
   methods: {
+    arquivoEnviado(info) {
+      const formatedData = JSON.parse(info.files[0].xhr.response);
+      this.form.photo = formatedData.file;
+
+      this.$q.notify({
+        'type': 'positive',
+        'message': 'O arquivo foi enviado com sucesso!',
+        'position': 'top-right'
+      })
+    },
     // axios
-    async saveData () {
+    async saveData() {
       const response = await AnimalService.create(this.form)
       if(response.status == '201'){
         this.$router.push('/animais')
--- src/pages/Animals/IndexPage.vue
+++ src/pages/Animals/IndexPage.vue
@@ -8,12 +8,12 @@
       <thead>
         <tr>
           <th class="text-left">ID</th>
+          <th class="text-left">Foto</th>
           <th class="text-left">Nome</th>
           <th class="text-left">Espécie</th>
           <th class="text-left">Raça</th>
           <th class="text-left">Tamanho</th>
           <th class="text-left">Idade</th>
-          <th class="text-left">Foto</th>
           <th class="text-left">Status</th>
           <th class="text-left">Descrição</th>
         </tr>
@@ -21,12 +21,16 @@
       <tbody>
         <tr v-for="(item, i) in animals" :key="i">
           <td class="text-left">{{ item.id }}</td>
+          <td class="text-left">
+            <q-avatar>
+              <q-img :src="item.photo" :ratio="1/1" />
+            </q-avatar>
+          </td>
           <td class="text-left"> {{ item.name }}</td>
-          <td class="text-left"> {{ item.specie }}</td>
+          <td class="text-left">{{ item.specie }}</td>
           <td class="text-left">{{ item.breed }}</td>
           <td class="text-left">{{ item.size }}</td>
           <td class="text-left">{{ item.age }}</td>
-          <td class="text-left">{{ item.photo }}</td>
           <td class="text-left">{{ item.status }}</td>
           <td class="text-left">{{ item.description }}</td>
           <td class="text-left">
